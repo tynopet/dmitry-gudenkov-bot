23f0479964e2841d6a78a6f984ee96fc8d70c78e
tynopet <dimangud@rambler.ru>
2017-09-21 12:28:02 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Telegraf = require('telegraf');
+const Telegraf = require("telegraf");
 
 const token = process.env.TOKEN;
 const host = process.env.HOST;
@@ -10,7 +10,7 @@ let sleep = false;
 bot.hears(/^Тихий час$/, (ctx, next) => {
   if (ctx.message.from.id === 89778872) {
     sleep = true;
-    ctx.reply('Бип');
+    ctx.reply("Бип");
   } else {
     return next();
   }
@@ -19,89 +19,119 @@ bot.hears(/^Тихий час$/, (ctx, next) => {
 bot.hears(/^Тихий час окончен$/, (ctx, next) => {
   if (ctx.message.from.id === 89778872) {
     sleep = false;
-    ctx.reply('Боп');
+    ctx.reply("Боп");
   } else {
     return next();
   }
 });
 
-bot.on('message', (ctx, next) => {
+bot.on("message", (ctx, next) => {
   if (sleep && ctx.update.message.from.id === 242046536) {
-    ctx.telegram.deleteMessage(ctx.update.message.chat.id, ctx.update.message.message_id);
+    ctx.telegram.deleteMessage(
+      ctx.update.message.chat.id,
+      ctx.update.message.message_id
+    );
   } else {
     return next();
   }
 });
 
-bot.on('edited_message', (ctx, next) => {
-  if (ctx.update.edited_message.from.id === 242046536 && ctx.update.edited_message.text
-    .match(/(6|б|b)+(\s|\.|_|\,)*(0|@|а|a|o|о|у|y|Fl|FI)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*(¥|у|y|u)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*/i)) {
-    ctx.telegram.deleteMessage(ctx.update.edited_message.chat.id, ctx.update.edited_message.message_id);
+bot.on("edited_message", (ctx, next) => {
+  if (
+    ctx.update.edited_message.from.id === 242046536 &&
+    ctx.update.edited_message.text.match(
+      /(6|б|b)+(\s|\.|_|\,)*(0|@|а|a|o|о|у|y|Fl|FI)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*(¥|у|y|u)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*/i
+    )
+  ) {
+    ctx.telegram.deleteMessage(
+      ctx.update.edited_message.chat.id,
+      ctx.update.edited_message.message_id
+    );
   }
 });
 
-bot.hears(/(6|б|b)+(\s|\.|_|\,)*(0|@|а|a|o|о|у|y|Fl|FI)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*(¥|у|y|u)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*/i, (ctx) => {
-  if (ctx.update.message.from.id === 242046536) {
-    ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+bot.hears(
+  /(6|б|b)+(\s|\.|_|\,)*(0|@|а|a|o|о|у|y|Fl|FI)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*(¥|у|y|u)*(\s|\.|_|\,)*(т|t)+(\s|\.|_|\,)*/i,
+  ctx => {
+    if (ctx.update.message.from.id === 242046536) {
+      ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+    }
   }
-});
+);
 
 bot.hears(/(\)){2,}/, (ctx, next) => {
-  if (Math.random() > 0.65) {
-    ctx.reply(')))0))0)', {
-      reply_to_message_id: ctx.message.message_id
-    });
+  if (ctx.chat.id == "-1001074297259") {
+    if (Math.random() > 0.65) {
+      ctx.reply(")))0))0)", {
+        reply_to_message_id: ctx.message.message_id
+      });
+    }
+    return next();
   }
-  return next();
 });
 
 bot.hears(/фр(о|o)нт(е|e)нд/i, (ctx, next) => {
-  if (ctx.chat.id == '-1001074297259') {
-    ctx.reply('Фронетнд работает. Всегда. Без исключений. Если у вас что-то не получается, вытащите руки из жопы, плес.', {
-      reply_to_message_id: ctx.message.message_id
-    });
+  if (ctx.chat.id == "-1001074297259") {
+    ctx.reply(
+      "Фронетнд работает. Всегда. Без исключений. Если у вас что-то не получается, вытащите руки из жопы, плес.",
+      {
+        reply_to_message_id: ctx.message.message_id
+      }
+    );
   }
   return next();
 });
 
 bot.hears(/тун(о|o)п(е|e)т/i, (ctx, next) => {
-  ctx.reply('Чо?');
-  return next();
+  if (ctx.chat.id == "-1001074297259") {
+    ctx.reply("Чо?");
+    return next();
+  }
 });
 
 bot.hears(/т(е|e)кст(а|a)йл/i, (ctx, next) => {
-  ctx.reply('Все нормально. В большом светлом будущем возможно будет маркдаун', {
-    reply_to_message_id: ctx.message.message_id
-  });
-  return next();
+  if (ctx.chat.id == "-1001074297259") {
+    ctx.reply(
+      "Все нормально. В большом светлом будущем возможно будет маркдаун",
+      {
+        reply_to_message_id: ctx.message.message_id
+      }
+    );
+    return next();
+  }
 });
 
 bot.hears(/м(а|a)т(е|e)рх(о|o)(с|c)т/i, (ctx, next) => {
-  ctx.reply('Метромост');
-  return next();
+  if (ctx.chat.id == "-1001074297259") {
+    ctx.reply("Метромост");
+    return next();
+  }
 });
 
 bot.hears(/в(е|e)р(с|c)(у|y)(с|c)/i, (ctx, next) => {
-  ctx.reply('Антихайп', {
+  ctx.reply("Антихайп", {
     reply_to_message_id: ctx.message.message_id
   });
   return next();
 });
 
 bot.hears(/(с|c)л(о|o)м(а|a)л(с|c)я/i, (ctx, next) => {
-  ctx.reply('Знаете, скорее всего, всё дело во мне. Я притягиваю неисправности, как магнит. Так как ваши жестянки рядом со мной глючат напропалую.', {
-    reply_to_message_id: ctx.message.message_id
-  });
+  ctx.reply(
+    "Знаете, скорее всего, всё дело во мне. Я притягиваю неисправности, как магнит. Так как ваши жестянки рядом со мной глючат напропалую.",
+    {
+      reply_to_message_id: ctx.message.message_id
+    }
+  );
   return next();
 });
 
 bot.hears(/дв(а|a)ч/i, (ctx, next) => {
-  if (ctx.chat.id == '-1001074297259') {
-    ctx.reply('Иди на хуй!', {
+  if (ctx.chat.id == "-1001074297259") {
+    ctx.reply("Иди на хуй!", {
       reply_to_message_id: ctx.message.message_id
     });
   }
 });
 
 bot.telegram.setWebhook(host);
-bot.startWebhook('/', null, port);
+bot.startWebhook("/", null, port);
